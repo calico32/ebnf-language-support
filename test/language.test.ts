1d0f0ea49042530179bd63c39b626e582eced969
Caleb Chan <cchan@hey.com>
2025-09-15 10:28:51 -0400

    add basic test

---
+++ test/language.test.ts
@@ -0,0 +1,145 @@
+import { test } from 'bun:test'
+import { readFileSync } from 'node:fs'
+import * as ast from '../language/ast'
+import { FileSet } from '../language/file'
+import { Parser } from '../language/parser'
+import {
+  $many,
+  alternation,
+  concatenation,
+  except,
+  grammar,
+  group,
+  ident,
+  type Matcher,
+  optional,
+  range,
+  repetition,
+  rule,
+  special,
+  string,
+} from './matchers'
+
+const ebnfGrammar = grammar([
+  rule('grammar', repetition(ident('rule'))),
+  rule(
+    'rule',
+    $many(concatenation, [
+      ident('name'),
+      string('='),
+      ident('expression'),
+      string(';'),
+    ])
+  ),
+  rule(
+    'name',
+    concatenation(
+      ident('name_start_character'),
+      repetition(ident('name_character'))
+    )
+  ),
+  rule(
+    'name_start_character',
+    $many(alternation, [ident('letter'), ident('digit'), string('_')])
+  ),
+  rule(
+    'name_character',
+    alternation(ident('name_start_character'), string('-'))
+  ),
+  rule(
+    'letter',
+    alternation(
+      range(string('a'), string('z')),
+      range(string('A'), string('Z'))
+    )
+  ),
+  rule('digit', range(string('0'), string('9'))),
+  rule(
+    'expression',
+    $many(alternation, [
+      ident('name'),
+      ident('literal'),
+      ident('special'),
+      ident('group'),
+      ident('repetition'),
+      ident('alternation'),
+      ident('concatenation'),
+      ident('optional'),
+      ident('range'),
+    ])
+  ),
+  rule(
+    'literal',
+    alternation(
+      group(
+        $many(concatenation, [
+          string('"'),
+          except(special('? any character ?'), string('"')),
+          string('"'),
+        ])
+      ),
+      group(
+        $many(concatenation, [
+          string("'"),
+          except(special('? any character ?'), string("'")),
+          string("'"),
+        ])
+      )
+    )
+  ),
+  rule(
+    'special',
+    $many(concatenation, [
+      string('?'),
+      except(special('? any character ?'), string('?')),
+      string('?'),
+    ])
+  ),
+  rule(
+    'group',
+    $many(concatenation, [string('('), ident('expression'), string(')')])
+  ),
+  rule(
+    'repetition',
+    $many(concatenation, [string('{'), ident('expression'), string('}')])
+  ),
+  rule(
+    'alternation',
+    $many(concatenation, [
+      ident('expression'),
+      string('|'),
+      ident('expression'),
+    ])
+  ),
+  rule(
+    'concatenation',
+    $many(concatenation, [
+      ident('expression'),
+      optional(string(',')),
+      ident('expression'),
+    ])
+  ),
+  rule(
+    'optional',
+    $many(concatenation, [string('['), ident('expression'), string(']')])
+  ),
+  rule(
+    'range',
+    $many(concatenation, [ident('literal'), string('..'), ident('literal')])
+  ),
+])
+
+const parseTests = [
+  [readFileSync('ebnf.ebnf', 'utf8'), ebnfGrammar],
+  [readFileSync('test/ebnf-no-commas.ebnf', 'utf8'), ebnfGrammar],
+] as [string, Matcher<ast.Grammar>][]
+
+function parse(input: string): ast.Grammar {
+  const fset = new FileSet()
+  const parser = new Parser(fset, 'test.ebnf', input)
+  return parser.parseGrammar()
+}
+
+test.each(parseTests)('parses %s', (input, matcher) => {
+  matcher(parse(input))
+})
---
+++ test/matchers.ts
@@ -0,0 +1,215 @@
+import { expect } from 'bun:test'
+import * as ast from '../language/ast'
+import * as token from '../language/token'
+
+const internal = Symbol('internal')
+export function $serialize(value: any): any | undefined {
+  if (internal in value) {
+    return value[internal]
+  }
+  return undefined
+}
+
+export type Matcher<T = ast.Expr> = (value: T) => void
+type ArrayAtLeast<
+  T,
+  MinLength extends number,
+  _A extends T[] = []
+> = _A['length'] extends MinLength
+  ? [..._A, ...T[]]
+  : ArrayAtLeast<T, MinLength, [..._A, T]>
+
+export function $many(
+  factory: (left: Matcher, right: Matcher) => Matcher,
+  values: ArrayAtLeast<Matcher, 2>
+): Matcher {
+  let m: Matcher<any> = values[0]
+  for (let i = 1; i < values.length; i++) {
+    m = factory(m, values[i])
+  }
+  return m
+}
+
+export function grammar(
+  ruleMatchers: Matcher<ast.Rule>[]
+): Matcher<ast.Grammar> {
+  const f = (grammar: ast.Grammar) => {
+    expect(grammar).toBeInstanceOf(ast.Grammar)
+    let unmatchedRules = grammar.rules.size
+    for (const ruleMatcher of ruleMatchers) {
+      const rule = grammar.rules.get(ruleMatcher.name)
+      if (!rule) {
+        expect().fail(`rule ${ruleMatcher.name} not found`)
+      } else {
+        ruleMatcher(rule)
+        unmatchedRules--
+      }
+    }
+
+    expect(unmatchedRules, 'unmatched rules').toBe(0)
+  }
+  Object.defineProperty(f, internal, {
+    value: { type: 'grammar', rules: ruleMatchers.map($serialize) },
+  })
+  return f
+}
+
+export function rule(name: string, valueMatcher: Matcher): Matcher<ast.Rule> {
+  const f = (rule: ast.Rule) => {
+    expect(rule).toBeInstanceOf(ast.Rule)
+    expect(rule.name.name).toBe(name)
+    valueMatcher(rule.value)
+  }
+  Object.defineProperty(f, 'name', { value: name })
+  Object.defineProperty(f, internal, {
+    value: { type: 'rule', name, value: $serialize(valueMatcher) },
+  })
+  return f
+}
+
+export function group(exprMatcher: Matcher): Matcher {
+  const f = (group: ast.Expr) => {
+    expect(group).toBeInstanceOf(ast.Group)
+    exprMatcher((group as ast.Group).expr)
+  }
+  Object.defineProperty(f, internal, {
+    value: { type: 'group', expr: $serialize(exprMatcher) },
+  })
+  return f
+}
+
+export function ident(name: string): Matcher {
+  const f = (expr: ast.Expr) => {
+    expect(expr).toBeInstanceOf(ast.Ident)
+    expect((expr as ast.Ident).name).toBe(name)
+  }
+  Object.defineProperty(f, internal, {
+    value: { type: 'ident', name },
+  })
+  return f
+}
+
+export function string(value: string): Matcher {
+  const f = (expr: ast.Expr) => {
+    expect(expr).toBeInstanceOf(ast.String)
+    expect((expr as ast.String).value.slice(1, -1)).toBe(value)
+  }
+  Object.defineProperty(f, internal, {
+    value: { type: 'string', value },
+  })
+  return f
+}
+
+export function special(value: string): Matcher {
+  const f = (expr: ast.Expr) => {
+    expect(expr).toBeInstanceOf(ast.Special)
+    expect((expr as ast.Special).value).toBe(value)
+  }
+  Object.defineProperty(f, internal, {
+    value: { type: 'special', value },
+  })
+  return f
+}
+
+export function repetition(exprMatcher: Matcher): Matcher {
+  const f = (expr: ast.Expr) => {
+    expect(expr).toBeInstanceOf(ast.Repitition)
+    exprMatcher((expr as ast.Repitition).expr)
+  }
+  Object.defineProperty(f, internal, {
+    value: { type: 'repetition', expr: $serialize(exprMatcher) },
+  })
+  return f
+}
+
+export function optional(exprMatcher: Matcher): Matcher {
+  const f = (expr: ast.Expr) => {
+    expect(expr).toBeInstanceOf(ast.Optional)
+    exprMatcher((expr as ast.Optional).expr)
+  }
+  Object.defineProperty(f, internal, {
+    value: { type: 'optional', expr: $serialize(exprMatcher) },
+  })
+  return f
+}
+
+export function binaryExpr(
+  op: token.Token,
+  leftMatcher: Matcher,
+  rightMatcher: Matcher
+): Matcher {
+  const f = (expr: ast.Expr) => {
+    expect(expr).toBeInstanceOf(ast.BinaryExpr)
+    expect((expr as ast.BinaryExpr).op).toBe(op)
+    leftMatcher((expr as ast.BinaryExpr).left)
+    rightMatcher((expr as ast.BinaryExpr).right)
+  }
+  Object.defineProperty(f, internal, {
+    value: {
+      type: 'binaryExpr',
+      op,
+      left: $serialize(leftMatcher),
+      right: $serialize(rightMatcher),
+    },
+    writable: true,
+  })
+  return f
+}
+
+export function concatenation(
+  leftMatcher: Matcher,
+  rightMatcher: Matcher
+): Matcher {
+  const f = binaryExpr(token.Token.Concatenate, leftMatcher, rightMatcher)
+  Object.defineProperty(f, internal, {
+    value: {
+      type: 'concatenation',
+      left: $serialize(leftMatcher),
+      right: $serialize(rightMatcher),
+    },
+    writable: false,
+  })
+  return f
+}
+
+export function alternation(
+  leftMatcher: Matcher,
+  rightMatcher: Matcher
+): Matcher {
+  const f = binaryExpr(token.Token.Alternate, leftMatcher, rightMatcher)
+  Object.defineProperty(f, internal, {
+    value: {
+      type: 'alternation',
+      left: $serialize(leftMatcher),
+      right: $serialize(rightMatcher),
+    },
+    writable: false,
+  })
+  return f
+}
+
+export function range(leftMatcher: Matcher, rightMatcher: Matcher): Matcher {
+  const f = binaryExpr(token.Token.Range, leftMatcher, rightMatcher)
+  Object.defineProperty(f, internal, {
+    value: {
+      type: 'range',
+      left: $serialize(leftMatcher),
+      right: $serialize(rightMatcher),
+    },
+    writable: false,
+  })
+  return f
+}
+
+export function except(leftMatcher: Matcher, rightMatcher: Matcher): Matcher {
+  const f = binaryExpr(token.Token.Except, leftMatcher, rightMatcher)
+  Object.defineProperty(f, internal, {
+    value: {
+      type: 'except',
+      left: $serialize(leftMatcher),
+      right: $serialize(rightMatcher),
+    },
+    writable: false,
+  })
+  return f
+}
