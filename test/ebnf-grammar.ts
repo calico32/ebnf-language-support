f5973f626d2f8e058d71da72da2ad1ab5be97142
Caleb Chan <cchan@hey.com>
2025-09-16 13:39:00 -0400

    grammar, test: cleanup style

---
+++ test/ebnf-grammar.ts
@@ -0,0 +1,146 @@
+import {
+  $many,
+  alternation,
+  concatenation,
+  except,
+  grammar,
+  group,
+  ident,
+  optional,
+  range,
+  repetition,
+  rule,
+  special,
+  string,
+} from './matchers'
+
+export const ebnfGrammar = grammar([
+  rule('grammar', repetition(alternation(ident('comment'), ident('rule')))),
+  rule(
+    'comment',
+    $many(concatenation, [
+      string('(*'),
+      except(special('? any string ?'), string('*)')),
+      string('*)'),
+    ])
+  ),
+  rule(
+    'rule',
+    $many(concatenation, [
+      ident('name'),
+      string('='),
+      ident('expression'),
+      string(';'),
+    ])
+  ),
+  rule(
+    'name',
+    concatenation(
+      ident('name-start-character'),
+      repetition(ident('name-character'))
+    )
+  ),
+  rule(
+    'name-start-character',
+    $many(alternation, [ident('letter'), ident('digit'), string('_')])
+  ),
+  rule(
+    'name-character',
+    alternation(ident('name-start-character'), string('-'))
+  ),
+  rule(
+    'letter',
+    alternation(
+      range(string('a'), string('z')),
+      range(string('A'), string('Z'))
+    )
+  ),
+  rule('digit', range(string('0'), string('9'))),
+  rule(
+    'expression',
+    $many(alternation, [
+      ident('name'),
+      ident('literal'),
+      ident('special'),
+      ident('group'),
+      ident('repetition'),
+      ident('optional'),
+      ident('alternation'),
+      ident('concatenation'),
+      ident('range'),
+      ident('except'),
+    ])
+  ),
+  rule(
+    'literal',
+    alternation(
+      group(
+        $many(concatenation, [
+          string('"'),
+          except(special('? any string ?'), string('"')),
+          string('"'),
+        ])
+      ),
+      group(
+        $many(concatenation, [
+          string("'"),
+          except(special('? any string ?'), string("'")),
+          string("'"),
+        ])
+      )
+    )
+  ),
+  rule(
+    'special',
+    $many(concatenation, [
+      string('?'),
+      except(special('? any string ?'), string('?')),
+      string('?'),
+    ])
+  ),
+  rule(
+    'group',
+    $many(concatenation, [string('('), ident('expression'), string(')')])
+  ),
+  rule(
+    'repetition',
+    $many(concatenation, [string('{'), ident('expression'), string('}')])
+  ),
+
+  rule(
+    'optional',
+    $many(concatenation, [string('['), ident('expression'), string(']')])
+  ),
+  rule(
+    'alternation',
+    $many(concatenation, [
+      ident('expression'),
+      string('|'),
+      ident('expression'),
+    ])
+  ),
+  rule(
+    'concatenation',
+    $many(concatenation, [
+      ident('expression'),
+      optional(string(',')),
+      ident('expression'),
+    ])
+  ),
+  rule(
+    'range',
+    $many(concatenation, [
+      ident('expression'),
+      string('..'),
+      ident('expression'),
+    ])
+  ),
+  rule(
+    'except',
+    $many(concatenation, [
+      ident('expression'),
+      string('-'),
+      ident('expression'),
+    ])
+  ),
+])
--- test/language.test.ts
+++ test/language.test.ts
@@ -3,144 +3,17 @@ import { readFileSync } from 'node:fs'
 import * as ast from '../language/ast'
 import { FileSet } from '../language/file'
 import { Parser } from '../language/parser'
-import {
-  $many,
-  alternation,
-  concatenation,
-  except,
-  grammar,
-  group,
-  ident,
-  type Matcher,
-  optional,
-  range,
-  repetition,
-  rule,
-  special,
-  string,
-} from './matchers'
-
-const ebnfGrammar = grammar([
-  rule('grammar', repetition(alternation(ident('comment'), ident('rule')))),
-  rule(
-    'comment',
-    $many(concatenation, [
-      string('(*'),
-      except(special('? any string ?'), string('*)')),
-      string('*)'),
-    ])
-  ),
-  rule(
-    'rule',
-    $many(concatenation, [
-      ident('name'),
-      string('='),
-      ident('expression'),
-      string(';'),
-    ])
-  ),
-  rule(
-    'name',
-    concatenation(
-      ident('name_start_character'),
-      repetition(ident('name_character'))
-    )
-  ),
-  rule(
-    'name_start_character',
-    $many(alternation, [ident('letter'), ident('digit'), string('_')])
-  ),
-  rule(
-    'name_character',
-    alternation(ident('name_start_character'), string('-'))
-  ),
-  rule(
-    'letter',
-    alternation(
-      range(string('a'), string('z')),
-      range(string('A'), string('Z'))
-    )
-  ),
-  rule('digit', range(string('0'), string('9'))),
-  rule(
-    'expression',
-    $many(alternation, [
-      ident('name'),
-      ident('literal'),
-      ident('special'),
-      ident('group'),
-      ident('repetition'),
-      ident('alternation'),
-      ident('concatenation'),
-      ident('optional'),
-      ident('range'),
-    ])
-  ),
-  rule(
-    'literal',
-    alternation(
-      group(
-        $many(concatenation, [
-          string('"'),
-          except(special('? any string ?'), string('"')),
-          string('"'),
-        ])
-      ),
-      group(
-        $many(concatenation, [
-          string("'"),
-          except(special('? any string ?'), string("'")),
-          string("'"),
-        ])
-      )
-    )
-  ),
-  rule(
-    'special',
-    $many(concatenation, [
-      string('?'),
-      except(special('? any string ?'), string('?')),
-      string('?'),
-    ])
-  ),
-  rule(
-    'group',
-    $many(concatenation, [string('('), ident('expression'), string(')')])
-  ),
-  rule(
-    'repetition',
-    $many(concatenation, [string('{'), ident('expression'), string('}')])
-  ),
-  rule(
-    'alternation',
-    $many(concatenation, [
-      ident('expression'),
-      string('|'),
-      ident('expression'),
-    ])
-  ),
-  rule(
-    'concatenation',
-    $many(concatenation, [
-      ident('expression'),
-      optional(string(',')),
-      ident('expression'),
-    ])
-  ),
-  rule(
-    'optional',
-    $many(concatenation, [string('['), ident('expression'), string(']')])
-  ),
-  rule(
-    'range',
-    $many(concatenation, [ident('literal'), string('..'), ident('literal')])
-  ),
-])
+import { ebnfGrammar } from './ebnf-grammar'
+import { type Matcher } from './matchers'
 
 const parseTests = [
-  [readFileSync('ebnf.ebnf', 'utf8'), ebnfGrammar],
-  [readFileSync('test/ebnf-no-commas.ebnf', 'utf8'), ebnfGrammar],
-] as [string, Matcher<ast.Grammar>][]
+  ['ebnf.ebnf', readFileSync('ebnf.ebnf', 'utf8'), ebnfGrammar],
+  [
+    'ebnf-no-commas.ebnf',
+    readFileSync('test/ebnf-no-commas.ebnf', 'utf8'),
+    ebnfGrammar,
+  ],
+] as [string, string, Matcher<ast.Grammar>][]
 
 function parse(input: string): ast.Grammar {
   const fset = new FileSet()
@@ -148,6 +21,9 @@ function parse(input: string): ast.Grammar {
   return parser.parseGrammar()
 }
 
-test.each(parseTests)('parses %s', (input, matcher) => {
-  matcher(parse(input))
-})
+for (let [label, input, matcher] of parseTests) {
+  label ||= input
+  test(`parses ${label}`, () => {
+    matcher(parse(input))
+  })
+}
